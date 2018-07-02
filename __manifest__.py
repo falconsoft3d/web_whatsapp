08d8c07e71e4fa987fae9ac5b9e7168c1c42d163
marlon <falconsoft.3d@gmail.com>
2018-07-01 23:01:13 -0400

    Inicial

---
+++ __manifest__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2017 Marlon Falcón Hernandez
+#    (<http://www.falconsolutions.cl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Whatsapp MFH',
+    'version': '10.0.0.1.0',
+    'author': "Falcón Solutions",
+    'maintainer': 'Falcon Solutions',
+    'website': 'http://www.falconsolutions.cl',
+    'license': 'AGPL-3',
+    'category': 'Zopim',
+    'summary': 'Web Login',
+    'description': """
+whatsapp
+=====================================================
+1-. whatsapp.\n
+""",
+    'data': [
+            'templates/whatsapp.xml',
+            ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+
+}
---
+++ models/res_company.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+class company_zopim(models.Model):
+
+    _description = "Company Zopim"
+    _inherit = 'res.company'
+
+    zopim = fields.Text('Zopim ID')
